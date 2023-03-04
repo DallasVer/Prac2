6b55e778a44cf055d26600256afecc7196fd5e41
DallasVer <118686739+DallasVer@users.noreply.github.com>
2023-03-04 15:01:38 +0700

    Add files via upload

---
+++ prak2-4/prak2-4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml.Linq;
+
+namespace prak2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter trick = new Counter();
+            ConsoleKeyInfo key;
+
+            Console.WriteLine("Инициализация счетчика '~~'\n 1: По умолчанию \n 2: Произвольное значение. \n 3: Выйти.");
+                int res = Convert.ToInt32(Console.ReadLine());
+                switch (res)
+                {
+                    case 1:
+                        Console.WriteLine("Введите первое значение");
+                        trick.Now = int.Parse(Console.ReadLine());
+                    break;
+                    case 2: 
+                        trick.Now = trick.Rand;
+                    break;
+                    case 3: return;
+                }
+            while (true)
+            {
+                Console.WriteLine("Операции со счётчиком: \n 1: Знать текущее состояние \n 2: Работать со счётчиком. \n 3: Выйти.");
+
+                res = Convert.ToInt32(Console.ReadLine());
+                switch (res)
+                {
+                    case 1: Console.WriteLine(trick.Now); break;
+                    case 2:
+                        Console.WriteLine(" KeyUp ↑︎: Продвинуться вверх \n KeyDown ↓︎: Продвинуться вниз \n Escape: Выйти.");
+                        key = Console.ReadKey(true);
+                            switch (key.Key)
+                            { 
+                                case ConsoleKey.DownArrow: trick.Down(); break;
+                                case ConsoleKey.UpArrow: trick.Up();  break;
+                                case ConsoleKey.Escape: break;
+                        }
+                        break;
+                    case 3: return;
+                }
+                
+
+            }
+        }
+    }
+}
---
+++ prak2-5/prak2-5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace prak2_5
+{
+    internal class Program
+    {
+        static void Test()
+        {
+            Construct del = new Construct();
+        }
+        static void Main(string[] args)
+        {
+            Construct Val = new Construct();
+
+            Console.WriteLine("Напишите два новых свойства.");
+            Console.WriteLine("Введите первое значение");
+            Val.con1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе значение");
+            Val.con2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1: Удалить оба пункта.");
+            int res = Convert.ToInt32(Console.ReadLine());
+                Test();
+                GC.Collect();
+                Console.ReadKey();
+            }
+        }
+    }
