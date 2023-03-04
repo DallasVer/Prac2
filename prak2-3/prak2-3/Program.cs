4c647b98d3c687a5fbdee23ea78c89fedd640a1a
DallasVer <118686739+DallasVer@users.noreply.github.com>
2023-03-04 14:59:51 +0700

    Add files via upload

---
+++ prak2-3/prak2-3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace prak2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person rub = new Person();
+            while (true)
+            {
+                Console.WriteLine("Выберите пункт \n 1: Вывести на экран числа. \n 2: Изменить числа. \n 3: Найти сумму чисел \n 4: Найти наибольшее из чисел. \n 5: Закрыть меню.");
+                int res = Convert.ToInt32(Console.ReadLine());
+                switch (res) {
+                    case 1: 
+                        rub.Vivod(); 
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите первое значение");
+                        rub.Zna1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите второе значение");
+                        rub.Zna2 = int.Parse(Console.ReadLine()); 
+                        break;
+                    case 3: 
+                        Console.WriteLine($" Сумма чисел {rub.Zna1} и {rub.Zna2} = {rub.Zna1 + rub.Zna2}");
+                        break;
+                    case 4: rub.more(); break;
+                    case 5: return;
+            }
+            }
+        }
+    }
+}
---
+++ prak2-3/prak2-3/prakClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak2_3
+{
+    public class Person
+    {
+        private int num1;
+        private int num2;
+        public int Zna1
+        {
+            get
+            { return this.num1; }
+            set
+            { this.num1 = value; }
+        }
+
+        public int Zna2
+        {
+            get
+            { return this.num2; }
+            set
+            { this.num2 = value; }
+        }
+        public Person()
+        {
+            Zna1 = 4;
+            Zna2 = 5;
+        }
+
+        public void Vivod()
+        {
+            Console.WriteLine($" Первое число = {Zna1} \n Второе число = {Zna2}");
+        }
+
+        public  void more()
+        {
+            if (Zna1 > Zna2)
+            {
+                Console.WriteLine($" Число {Zna1} больше числа {Zna2}");
+            }
+            else if (Zna1 < Zna2)
+            {
+                Console.WriteLine($" Число {Zna2} больше числа {Zna1}");
+            }
+            else
+            {
+                Console.WriteLine("Они индифферентны");
+            }
+        }
+   }
+}
